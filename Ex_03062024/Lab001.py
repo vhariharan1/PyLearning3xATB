03f75801f8c28dec4528e6aadaaea2716bf5cf9f
hariharan-viswanathan-glean <hariharan.viswanathan@glean.com>
2024-06-09 19:28:57 +0530

    Learning sessions 1 to 3

---
+++ Ex_03062024/Lab001.py
@@ -0,0 +1,25 @@
+# First file created for practise
+print("Hello there", end='_')
+print("How are you?")
+
+# r1 = input("ENter 1st number: ")
+# r2 = input("Enter 2nd number: ")
+
+# print(int(r1)+int(r2))
+
+fname = "Harry"
+lname = "Potter"
+print(fname, lname)
+print(fname + " " + lname)
+print(f"My name is {fname}{lname}")
+
+num = 13
+print(f"{num}*5={num*5}")
+
+# b = 1
+# print("2x{}={},{}".format(*args:b,b*2,3))
+
+my_list = [1, 2, 3, 4, 5]
+my_list.pop(1)
+my_list.pop()
+print(my_list)
